9d437a8232d53d29ae67306b8bc775721f7bb8bb
Mathias Vagni <mathiasvagni@googlemail.com>
2017-05-15 00:17:18 +0100

    Improved config validation & error messages.

--- src/lib/config.js
+++ src/lib/config.js
@@ -1,24 +1,24 @@
 const path = require('path');
 
-const CONFIG_SCHEMA = {
-    root: [
-        isArray(1),
-        {
-            name: isString(1),
-            templatePath: isPath(),
-            outputPath: isPath(),
-            fields: [
-                isArray(1),
-                {
-                    templateVariable: isString(1),
-                    question: isString(1),
-                    errorMessage: isOptional(isString(1)),
-                    isValid: isOptional(isFunction()),
-                },
-            ],
-        },
-    ],
-};
+const ROOT_KEY = Math.random() * 16 + '__root';
+
+const CONFIG_SCHEMA = [
+    isArray(1),
+    {
+        name: isString(1),
+        templatePath: isPath(),
+        outputPath: isPath(),
+        fields: [
+            isArray(1),
+            {
+                templateVariable: isString(1),
+                question: isString(1),
+                errorMessage: isOptional(isString(1)),
+                isValid: isOptional(isFunction()),
+            },
+        ],
+    },
+];
 
 function getConfig() {
     const pwd = process.env.PWD;
@@ -31,39 +31,56 @@ function getTemplateConfig(templateName) {
     return config.find(c => c.name === templateName);
 }
 
-function isString(minLength = 0) {
+function errorPrefix(key) {
+    if (key === ROOT_KEY) {
+        return 'Your config';
+    }
+    return `In your config, "${key}"`;
+}
+
+function errorReceived(val) {
+    // Node console.logs arrays as just their values. That means
+    // that without this an array with a single string is indistinguishable
+    // from a string when console logged.
+    if (Array.isArray(val)){
+        return `Received an array: ${val}`;
+    }
+    return `Received: ${val}`;
+}
+
+function isString(minLength = 0, helpText) {
     return (key, val) => {
-        if (typeof val === "string" && val.length >= 0) {
+        if (typeof val === "string" && val.length >= minLength) {
             return;
         };
-        throw new Error(`'${key}' should be a string at least ${minLength} characters in length. Received: ${val}`);
+        throw new Error(`${errorPrefix(key)} should be a string at least ${minLength} characters in length. ${errorReceived(val)}`);
     }
 }
 
-function isPath() {
+function isPath(helpText) {
     return (key, val) => {
         if (typeof val === "string" && val) {
             return;
         };
-        throw new Error(`'${key}' should be a should be a path (string). Received: ${val}`);
+        throw new Error(`${errorPrefix(key)} should be a should be a path (string). ${errorReceived(val)}`);
     }
 }
 
-function isFunction(val) {
+function isFunction(val, helpText) {
     return (key, val) => {
         if (typeof val === "function") {
             return;
         };
-        throw new Error(`'${key}' should be a function. Received: ${val}`);
+        throw new Error(`${errorPrefix(key)} should be a function. ${errorReceived(val)}`);
     }
 }
 
-function isArray(minLength = 0) {
+function isArray(minLength = 0, helpText) {
     return (key, val) => {
         if (Array.isArray(val, (minLength = 1))) {
             return;
         };
-        throw new Error(`'${key}' should be an array. Received: ${val}`);
+        throw new Error(`${errorPrefix(key)} should be an array. ${errorReceived(val)}`);
     }
 }
 
@@ -71,40 +88,30 @@ function isOptional(fieldTypeFunc) {
     return (key, val) => typeof val === "undefined" || fieldTypeFunc(key, val);
 }
 
-function validate(schema, config) {
+function validate(schema, config, key=ROOT_KEY) {
+
+    if (Array.isArray(schema)) {
+        let [isValid, subSchema] = schema;
+        isValid(key, config);
+        config.forEach(c => validate(subSchema, c));
+        return;
+    }
 
     Object.keys(schema).forEach(key => {
-        const currentSchema = schema[key];
+        const currentConfigValue = config[key];
 
-        // If it's the top level of the schema
-        if (key === 'root') {
-            let [isValid, subSchema] = currentSchema;
-            isValid(key, config);
-            config.forEach(c => validate(subSchema, c));
-        }
-        // If it's an array
-        else if (Array.isArray(currentSchema)) {
-            let [isValid, subSchema] = currentSchema;
-            isValid(key, config[key]);
-            config[key].forEach(c => validate(subSchema, c));
-        }
-        // It's a function
-        else {
+        if (Array.isArray(schema[key])) {
+           validate(schema[key], currentConfigValue, key);
+        } else {
             let isValid = schema[key];
-            isValid(key, config[key]);
+            isValid(key, currentConfigValue);
         }
     });
 }
 
 function validateConfig(config) {
-
-    try {
-        validate(CONFIG_SCHEMA, config);
-        return config;
-    }
-    catch (e) {
-        throw new Error(`In your config, ${e.message}`);
-    }
+    validate(CONFIG_SCHEMA, config);
+    return config;
 }
 
 module.exports = {
